479456cf4b4313950474dfdd300f8a473038365b
Paweł Müller <pawelmuller42@gmail.com>
2022-12-10 16:01:54 +0100

    Added colors class

---
+++ components/constants.py
@@ -0,0 +1,23 @@
+class Colors:
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+
+    red = (255, 0, 0)
+    green = (0, 255, 0)
+    blue = (0, 0, 255)
+
+    cyan = (0, 255, 255)
+    magenta = (255, 0, 255)
+    yellow = (255, 255, 0)
+
+    # Backgrounds
+    background_dark = (16, 23, 38)
+    background_semi_dark = (38, 41, 61)
+
+    # Contrasting
+    contrast_light = (234, 235, 252)
+    contrast_light_blue = (162, 218, 240)
+    contrast_blue = (78, 172, 210)
+
+    # Special
+    amp_orange = (236, 103, 37)
