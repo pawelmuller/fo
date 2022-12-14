efb7a91e0107e742c362d9d4951a60f05f5915a0
Paweł Müller <pawelmuller42@gmail.com>
2022-12-14 21:16:57 +0100

    Improved scaling the chart

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ SURFACE_HEIGHT = 720
 
 animation_rectangle_width = SURFACE_WIDTH
 animation_rectangle_height = 420
+animation_coordination_system_origin = (0.1 * animation_rectangle_width, 0.5 * animation_rectangle_height)
+
 control_rectangle_width = SURFACE_WIDTH
 control_rectangle_height = 300
 
@@ -38,6 +40,18 @@ def draw_coordination_system(screen):
                  width=3)
 
 
+def translate_graph_to_global_coordinates(points: [(float, float)]) -> [(float, float)]:
+    translated_points = []
+    origin_x, origin_y = animation_coordination_system_origin
+    for x, y in points:
+        translated_point = (
+            x * 0.8 * animation_rectangle_width / 2. / pi + origin_x,
+            y * 0.8 * animation_rectangle_height / 4. + origin_y
+        )
+        translated_points.append(translated_point)
+    return translated_points
+
+
 def wave_equation(x, amplitude, wave_length, omega, time):
     k = 2 * pi / wave_length
     y = 2 * amplitude * sin(k * x) * cos(omega * time)
@@ -48,11 +62,6 @@ def main():
     pg.init()
 
     screen = pg.display.set_mode((SURFACE_WIDTH, SURFACE_HEIGHT))
-    # Temp grid
-    for i in range(11):
-        pg.draw.line(screen, (90, 90, 90), (animation_rectangle_width / 10 * i, 0), (animation_rectangle_width / 10 * i, animation_rectangle_height))
-        pg.draw.line(screen, (90, 90, 90), (0, animation_rectangle_height / 10 * i), (animation_rectangle_width, animation_rectangle_height / 10 * i))
-
 
     clock = pg.time.Clock()
 
@@ -69,19 +78,24 @@ def main():
         points = []
         for i in range(POINTS_AMOUNT):
             x = i * 2. * pi / POINTS_AMOUNT
-            amplitude = 0.1
-            point = (x, wave_equation(x, amplitude, 1., 1., t))
-
-            translated_point = (point[0] * 0.8 * animation_rectangle_width / 6.25 + 0.1 * animation_rectangle_width,
-                                point[1] * 0.8 * animation_rectangle_height + 0.5 * animation_rectangle_height)
-            points.append(translated_point)
+            amplitude = 0.25
+            point = (x, wave_equation(x, amplitude, 1, 0.1, t))
+            points.append(point)
 
         # Translate local coordinates to global system
+        translated_points = translate_graph_to_global_coordinates(points)
 
         # Render the graphics here.
         draw_main_components(screen=screen)
         draw_coordination_system(screen=screen)
-        pg.draw.lines(surface=screen, color=Colors.amp_orange, closed=False, points=points, width=2)
+        # Temp grid
+        for i in range(11):
+            pg.draw.line(screen, (90, 90, 90), (animation_rectangle_width / 10 * i, 0),
+                         (animation_rectangle_width / 10 * i, animation_rectangle_height))
+            pg.draw.line(screen, (90, 90, 90), (0, animation_rectangle_height / 10 * i),
+                         (animation_rectangle_width, animation_rectangle_height / 10 * i))
+
+        pg.draw.lines(surface=screen, color=Colors.amp_orange, closed=False, points=translated_points, width=2)
 
         pg.display.flip()  # Refresh on-screen display
         clock.tick(60)  # wait until next frame (at 60 FPS)
